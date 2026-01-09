6521505d48cfe15863f07780278b901f396f7e89
Bjoern Schilberg <bjoern.schilberg@posteo.de>
2026-01-09 19:35:04 +0100

    main.ts: Added comments

--- main.ts
+++ main.ts
@@ -1,16 +1,25 @@
 let y = 0
 let x = 0
+
+// Schwellenwert für die Erkennung der Bewegungsrichtung.
+// Nur für LED Anzeige relevant.
 let threshold_x = 200
+// Schwellenwert für die Erkennung der Bewegungsrichtung.
+// Nur für LED Anzeige relevant.
 let threshold_y = 200
+
 serial.redirect(
 SerialPin.USB_TX,
 SerialPin.USB_RX,
 BaudRate.BaudRate115200
 )
+
 basic.forever(function () {
     x = input.acceleration(Dimension.X)
     y = input.acceleration(Dimension.Y)
     serial.writeLine("" + x + "," + y)
+
+    // Optional: Anzeige der Richtungspfeile für LED Anzeige
     if (y < 0 - threshold_y) {
         if (x < 0 - threshold_x) {
             basic.showArrow(ArrowNames.NorthWest)
